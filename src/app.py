0bc2498fcbfd341c96f8a802c3b3659ad9c9ac45
Anirban Ghosh <anirbanghosh.07@gmail.com>
2021-12-10 01:29:01 +0530

    reformat code (#2)
    * reformat code
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * Update circleci config
    
    * change in circleci config file
    
    * change in circleci config file
    
    * change in circleci config file
    
    * change in circleci config file
    
    * Updated config.yml
    
    * change in circleci config file
    
    * Updated config.yml
    
    Co-authored-by: Anirban Ghosh <anghosh2@cisco.com>

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO)
     log = logging.getLogger(name='APP')
+
     log.info('CFM started')
 
     print('Welcome to Compressed File Manager(CFM)')
